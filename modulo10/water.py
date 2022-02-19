bc07670412356e7cbf39ecd1e6256cf36ae201ea
Apolinar Fernandez <afcpolo1983@gmail.com>
2022-02-19 01:18:53 -0500

    ejercicios terminados modulo 10

--- modulo10/water.py
+++ modulo10/water.py
@@ -1,10 +1,8 @@
-water_left(5, 100, 2)
-Traceback (most recent call last):
-  File "<stdin>", line 1, in <module>
-  File "<stdin>", line 6, in water_left
-RuntimeError: There is not enough water for 5 astronauts after 2 days!
-
-try:
+def water_left(astronauts, water_left, days_left):
+    daily_usage = astronauts * 11
+    total_usage = daily_usage * days_left
+    total_water_left = water_left - total_usage
+    return f"Total water left after {days_left} days is: {total_water_left} liters"
+    
     water_left(5, 100, 2)
-except RuntimeError as err:
-    alert_navigation_system(err)
+  
